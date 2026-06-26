cfdbef3f77cbdd87ce37182bc29a8d93b10a8e27
MariaJuliaSenaa <mariajuliasena0110@gmail.com>
2026-06-26 11:56:01 -0300

    Limpeza: Remoção de ficheiros de teste temporários

--- teste_mapa.py
+++
@@ -1,9 +0,0 @@
-import geopandas as gpd
-
-caminho_shp = 'mapa_df/limite_do_distrito_federal.shp'
-mapa_df = gpd.read_file(caminho_shp)
-
-print("--- Colunas do Mapa do DF ---")
-print(mapa_df.columns)
-print("\n--- Primeiras linhas ---")
-print(mapa_df.head(3))
